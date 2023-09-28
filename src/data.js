7601c104a09bf894a099b9110118ecc67b6cb166
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-09-28 18:20:29 +0100

    Fetch data from weatherapi

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import { BadRequest } from "./error";
+
+const Data = (() => {
+  const loadJson = async (query) => {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=6fa9bd8888834bd589d202014232309&q=${query}`
+    );
+    if (response.status == 200) {
+      return response.json();
+    } else {
+      throw new BadRequest(response);
+    }
+  };
+
+  return { loadJson };
+})();
+
+export { Data };
