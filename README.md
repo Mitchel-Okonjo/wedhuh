46179b01b33de95139eaef248061272053deba65
Mitchel-Okonjo <111641404+Mitchel-Okonjo@users.noreply.github.com>
2023-09-29 17:19:54 +0100

    Add web link

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A weather app made with HTML, CSS and JavaScript utilizing the weatherAPI for accurate, real-time data.
+A weather app made with HTML, CSS and JavaScript utilizing the weatherAPI for accurate, real-time data. [View live demo](https://mitchel-okonjo.github.io/wedhuh/)
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ import { BadRequest } from "./error";
 const Data = (() => {
   const loadJson = async (query) => {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=6fa9bd8888834bd589d202014232309&q=${query}`
+      `https://api.weatherapi.com/v1/current.json?key=6fa9bd8888834bd589d202014232309&q=${query}`
     );
     if (response.status == 200) {
       return response.json();
